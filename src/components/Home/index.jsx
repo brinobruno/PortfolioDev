1e6c6e49baa11c811f208e8caabac446ac8dc3da
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-12-11 11:38:29 -0300

    set up react-modal

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+
 import { Container, Content, Text, Title, Subtitle, DownloadHome } from './styles'
 import brunoImg from '../../assets/images/brunoImg.png'
 
 import downloadIcon from '../../assets/userIcons/download.svg'
 
+Modal.setAppElement('#root') //accessibility code recommended by documentation
+
 export function Home() {
+  const [isDownloadCVModalOpen, setIsDownloadCVModalOpen] = useState(false)
+
+  function handleOpenDownloadCVModal() {
+    setIsDownloadCVModalOpen(true)
+  }
+
+  function handleCloseDownloadCVModal() {
+    setIsDownloadCVModalOpen(false)
+  }
+
   return (
     <>
     <Container>
@@ -11,7 +26,13 @@ export function Home() {
         <Text>
           <Title>Ciao!<br />I'm Bruno</Title>
           <Subtitle>A future fullstack developer</Subtitle>
-          <DownloadHome id="download-home" >Resumé <img src={downloadIcon} alt="Ícone de download" /></DownloadHome>
+          <DownloadHome onClick={handleOpenDownloadCVModal} id="download-home" >Resumé <img src={downloadIcon} alt="Ícone de download" /></DownloadHome>
+          <Modal
+          isOpen={isDownloadCVModalOpen}
+          onRequestClose={handleCloseDownloadCVModal}
+          >
+            <h3>Download my Resumé</h3>
+          </Modal>
         </Text>
         <img id="bruno-img" src={brunoImg} alt="Foto de Bruno Corrêa" />
       </Content>
