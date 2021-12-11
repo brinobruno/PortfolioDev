1d3f01d8feb6ec389e8db1f35640f3a481fc4a74
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-12-11 11:02:56 -0300

    home section and responsive to widths

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,20 @@
-import { Container, Content } from './styles'
+import { Container, Content, Text, Title, Subtitle, DownloadHome } from './styles'
+import brunoImg from '../../assets/images/brunoImg.png'
+
+import downloadIcon from '../../assets/userIcons/download.svg'
 
 export function Home() {
   return (
     <>
     <Container>
+      <Content>
+        <Text>
+          <Title>Ciao!<br />I'm Bruno</Title>
+          <Subtitle>A future fullstack developer</Subtitle>
+          <DownloadHome id="download-home" >Resumé <img src={downloadIcon} alt="Ícone de download" /></DownloadHome>
+        </Text>
+        <img id="bruno-img" src={brunoImg} alt="Foto de Bruno Corrêa" />
+      </Content>
     </Container>
     </>
   )
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,5 +1,76 @@
 import styled from 'styled-components'
+import './styles.css'
 
 export const Container = styled.section `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   padding-top: 6.18rem; //99px
+  height: 100vh;
+  max-width: 900px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media screen and (max-width: 968px) {
+    max-width: 730px;
+  }
+`
+
+export const Content = styled.div `
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (max-width: 968px) {
+    #bruno-img {
+      width: 19rem;
+    }
+
+    @media screen and (max-width: 590px) {
+    #bruno-img {
+      width: 22rem;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    justify-content: space-around;
+  }
+
+  @media screen and (max-width: 590px) {
+    flex-direction: column-reverse;
+  }
+`
+
+export const Text = styled.div ``
+
+export const Title = styled.h1 `
+  font-size: var(--opening-font);
+  color: var(--body-text-color);
+  line-height: 5rem;
+
+  @media screen and (max-width: 768px) {
+    font-size: 4rem;
+  }
+`
+
+export const Subtitle = styled.h4 `
+  font-size: var(--opening-subtitle);
+  color: var(--primary-color);
+  font-weight: 400;
+
+  margin-bottom: 1.188rem;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.43rem;
+  }
+`
+
+export const DownloadHome = styled.button `
+  font-size: 1.315rem;
+  font-weight: 700;
+  width: 11.5rem;
+  height: 2.875rem;
+  border-radius: 6px;
 `
