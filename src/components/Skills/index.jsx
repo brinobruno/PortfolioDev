a903e4b6c57ba508e5b1f2a0b51262e3cd5991e7
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-12-13 10:38:56 -0300

    skills section complete but responsive

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,13 +1,129 @@
-import { Container, Content, Text, Title } from './styles'
+import { Container, Content, Title, Code, SkillsGrid, SkillsItem } from './styles'
+
+import javascriptIcon from '../../assets/skillsIcons/01-javascript.svg'
+import reactIcon from '../../assets/skillsIcons/02-react.svg'
+import nodeIcon from '../../assets/skillsIcons/03-node.svg'
+import expressIcon from '../../assets/skillsIcons/04-express.svg'
+import gitIcon from '../../assets/skillsIcons/05-git.svg'
+import sqlIcon from '../../assets/skillsIcons/06-sql.svg'
+import htmlIcon from '../../assets/skillsIcons/07-html.svg'
+import sassIcon from '../../assets/skillsIcons/08-sass.svg'
 
 export function Skills() {
   return (
     <Container>
       <Content>
+        <div>
         <Title>Skills</Title>
-          <Text>
-            
-          </Text>
+        <Code>&#60;ul id=”theMostRelevant”&#62;&#60;/ul&#62;</Code>
+        </div>
+          <SkillsGrid>
+            <SkillsItem>
+              <img src={javascriptIcon} alt="" />
+              <div>
+                <div className="tag-divider">
+                  <p className="typeTag">Frontend</p>
+                  <p className="typeTag">Backend</p>
+                </div>
+              <div className="description-wrapper">
+                <h4>Javascript</h4>
+                <p>Programming language</p>
+              </div>
+              </div>
+            </SkillsItem>
+
+            <SkillsItem>
+              <img src={gitIcon} alt="" />
+              <div>
+                <div className="tag-divider">
+                  <p className="typeTag">Versioning</p>
+                </div>
+              <div className="description-wrapper">
+                <h4>Git</h4>
+                <p>Version control system</p>
+              </div>
+              </div>
+            </SkillsItem>
+
+            <SkillsItem>
+              <img src={reactIcon} alt="" />
+              <div>
+                <div className="tag-divider">
+                  <p className="typeTag">Frontend</p>
+                </div>
+              <div className="description-wrapper">
+                <h4>ReactJS</h4>
+                <p>JavaScript UI library</p>
+              </div>
+              </div>
+            </SkillsItem>
+
+            <SkillsItem>
+              <img src={sqlIcon} alt="" />
+              <div>
+                <div className="tag-divider">
+                  <p className="typeTag">Database</p>
+                </div>
+              <div className="description-wrapper">
+                <h4>SQL</h4>
+                <p>Structured Query Language</p>
+              </div>
+              </div>
+            </SkillsItem>
+
+            <SkillsItem>
+              <img src={nodeIcon} alt="" />
+              <div>
+                <div className="tag-divider">
+                  <p className="typeTag">Backend</p>
+                </div>
+              <div className="description-wrapper">
+                <h4>NodeJS</h4>
+                <p>JavaScript runtime</p>
+              </div>
+              </div>
+            </SkillsItem>
+
+            <SkillsItem>
+              <img src={htmlIcon} alt="" />
+              <div>
+                <div className="tag-divider">
+                  <p className="typeTag">Frontend</p>
+                </div>
+              <div className="description-wrapper">
+                <h4>HTML5</h4>
+                <p>Hypertext Markup Language</p>
+              </div>
+              </div>
+            </SkillsItem>
+
+            <SkillsItem>
+              <img src={expressIcon} alt="" />
+              <div>
+                <div className="tag-divider">
+                  <p className="typeTag">Backend</p>
+                </div>
+              <div className="description-wrapper">
+                <h4>ExpressJS</h4>
+                <p>NodeJS framework</p>
+              </div>
+              </div>
+            </SkillsItem>
+
+            <SkillsItem>
+              <img src={sassIcon} alt="" />
+              <div>
+                <div className="tag-divider">
+                  <p className="typeTag">Frontend</p>
+                </div>
+              <div className="description-wrapper">
+                <h4>Sass</h4>
+                <p>CSS preprocessor</p>
+              </div>
+              </div>
+            </SkillsItem>
+          </SkillsGrid>
+          <Code>&#60;details&#62;I also code C++ but don’t tell anyone&#60;/details&#62;</Code>
       </Content>
     </Container>
   )
--- src/components/Skills/styles.js
+++ src/components/Skills/styles.js
@@ -27,11 +27,77 @@ export const Content = styled.div `
   }
 `
 
-export const Text = styled.div `
-  padding-top: 5.313rem;
-`
-
 export const Title = styled.h2 `
   font-size: var(--heading-title);
   color: var(--primary-color);
+  text-align: center;
+  `
+
+export const Code = styled.span `
+  text-align: center;
+  `
+
+export const SkillsGrid = styled.div `
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 1.25rem;
+  grid-row-gap: 1.25rem;
+
+  padding: 1.688rem 0;
+`
+
+export const SkillsItem = styled.div `
+  display: flex;
+  align-items: center;
+
+  width: 27.625rem;
+  height: 5.875rem;
+  
+  background-color: #1A1A1A;
+  padding: 1.5rem;
+
+  cursor: pointer;
+
+  &:hover {
+    transition: 0.3s ease;
+    background-color: #343434;
+
+      .typeTag {
+        transition: 0.3s ease;
+        background-color: #202020;
+      }
+  }
+
+  img {
+    padding-right: 0.875rem; 
+  }
+
+  .tag-divider {
+    display: flex;
+    gap: 0.313rem;
+
+    p {
+      font-size: 0.813rem;
+      color: var(--primary-color);
+      background-color: #595959;
+      border-radius: 0.625rem;
+      padding: 0 0.43rem;
+    }
+  }
+
+  .description-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 0.875rem;
+
+    h4 {
+      font-size: 1.5rem;
+      font-weight: 400;
+    }
+
+    p {
+      padding-top: 0.6rem;
+      color: #979797;
+    }
+  }
 `
