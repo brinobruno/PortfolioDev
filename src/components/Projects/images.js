a5dc4305f75040bceeefa821dbf1e04faebbbf90
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-12-13 22:45:10 -0300

    display selected project complete but repository links

---
+++ src/components/Projects/images.js
@@ -0,0 +1,10 @@
+import img00 from '../../assets/projectsImg/00-project.png'
+import img01 from '../../assets/projectsImg/01-project.png'
+import img02 from '../../assets/projectsImg/02-project.png'
+import img03 from '../../assets/projectsImg/03-project.png'
+import img04 from '../../assets/projectsImg/04-project.png'
+import img05 from '../../assets/projectsImg/05-project.png'
+
+const Images = [img00, img01, img02, img03, img04, img05]
+
+export default Images
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
+
 import { Container, Content, Title, Code, ProjectsContainer } from './styles'
-import crossfitImg from '../../assets/images/01-crossfitUncompressed.png'
+
+import Images from './images';
 
 export function Projects() {
+  const [selectedProject, setSelectedProject] = useState(Images[0])
+
   return (
     <Container>
       <Content>
@@ -9,7 +14,7 @@ export function Projects() {
         <Code>SELECT &#60;project&#62; FROM &#60;projects_list&#62;</Code>
         <ProjectsContainer>
           <div className='project-display'>
-            <img src={crossfitImg} alt='Crossfit project website' />
+            <img src={selectedProject} alt="Screenshot of the project selected" />
           <div>
             <button>View Project</button>
             <button>View Repository</button>
@@ -17,12 +22,12 @@ export function Projects() {
           </div>
 
           <div className='projects-list'>
-          <a href="">Crossfit Gym Website_</a>
-          <a href="">Candy Store Website_</a>
-          <a href="">React Todo App_</a>
-          <a href="">Snake Game_</a>
-          <a href="">JSON Placeholder_</a>
-          <a href="">Personal Website_</a>
+          <a onClick={() => { setSelectedProject(Images[0]) }}>Crossfit Gym Website_</a>
+          <a onClick={() => { setSelectedProject(Images[1]) }}>Candy Store Website_</a>
+          <a onClick={() => { setSelectedProject(Images[2]) }}>React Todo App_</a>
+          <a onClick={() => { setSelectedProject(Images[3]) }}>Snake Game_</a>
+          <a onClick={() => { setSelectedProject(Images[4]) }}>JSON Placeholder_</a>
+          <a onClick={() => { setSelectedProject(Images[5]) }}>Personal Website_</a>
           </div>
         </ProjectsContainer>
       </Content>
