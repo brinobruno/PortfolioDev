88eef90b1d19fa7ed4558f19c96a5e4311fe448b
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-12-14 23:15:05 -0300

    button switches project and repository urls

--- src/components/Projects/images.js
+++ src/components/Projects/images.js
@@ -5,6 +5,37 @@ import img03 from '../../assets/projectsImg/03-project.png'
 import img04 from '../../assets/projectsImg/04-project.png'
 import img05 from '../../assets/projectsImg/05-project.png'
 
-const Images = [img00, img01, img02, img03, img04, img05]
+const Images = {
+  imgItem00: {
+    image: img00,
+    projectUrl: 'https://crossfitatos.com.br/primeiropasso',
+    repositoryUrl: 'https://github.com/brinobruno/atosbox-landingpage-01'
+  },
+  imgItem01: {
+    image: img01,
+    projectUrl: 'https://da-so.vercel.app/',
+    repositoryUrl: 'https://github.com/brinobruno/DASO-confeitaria'
+  },
+  imgItem02: {
+    image: img02,
+    projectUrl: 'https://main.d1umw160g6fy01.amplifyapp.com',
+    repositoryUrl: 'https://github.com/brinobruno/React-Tasks-List'
+  },
+  imgItem03: {
+    image: img03,
+    projectUrl: 'https://distracted-beaver-2aa861.netlify.app/',
+    repositoryUrl: 'https://github.com/brinobruno/Jogo-da-Cobrinha'
+  },
+  imgItem04: {
+    image: img04,
+    projectUrl: 'https://json-io.vercel.app/',
+    repositoryUrl: 'https://github.com/brinobruno/JSONPlaceholder'
+  },
+  imgItem05: {
+    image: img05,
+    projectUrl: 'https://meuport.com/',
+    repositoryUrl: 'https://github.com/brinobruno/portfolio-MeuPort'
+  },
+}
 
 export default Images
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -5,7 +5,7 @@ import { Container, Content, Title, Code, ProjectsContainer } from './styles'
 import Images from './images';
 
 export function Projects() {
-  const [selectedProject, setSelectedProject] = useState(Images[0])
+  const [selectedProject, setSelectedProject] = useState(Images.imgItem00)
 
   return (
     <Container>
@@ -14,20 +14,20 @@ export function Projects() {
         <Code>SELECT &#60;project&#62; FROM &#60;projects_list&#62;</Code>
         <ProjectsContainer>
           <div className='project-display'>
-            <img src={selectedProject} alt="Screenshot of the project selected" />
+            <img src={selectedProject.image} alt="Screenshot of the project selected" />
           <div>
-            <button>View Project</button>
-            <button>View Repository</button>
+          <a href={selectedProject.projectUrl}><button>View Project</button></a>
+          <a href={selectedProject.repositoryUrl}><button>View Repository</button></a>
           </div>
           </div>
 
           <div className='projects-list'>
-          <a onClick={() => { setSelectedProject(Images[0]) }}>Crossfit Gym Website_</a>
-          <a onClick={() => { setSelectedProject(Images[1]) }}>Candy Store Website_</a>
-          <a onClick={() => { setSelectedProject(Images[2]) }}>React Todo App_</a>
-          <a onClick={() => { setSelectedProject(Images[3]) }}>Snake Game_</a>
-          <a onClick={() => { setSelectedProject(Images[4]) }}>JSON Placeholder_</a>
-          <a onClick={() => { setSelectedProject(Images[5]) }}>Personal Website_</a>
+          <a onClick={() => { setSelectedProject(Images.imgItem00) }}>Crossfit Gym Website_</a>
+          <a onClick={() => { setSelectedProject(Images.imgItem01) }}>Candy Store Website_</a>
+          <a onClick={() => { setSelectedProject(Images.imgItem02) }}>React Todo App_</a>
+          <a onClick={() => { setSelectedProject(Images.imgItem03) }}>Snake Game_</a>
+          <a onClick={() => { setSelectedProject(Images.imgItem04) }}>JSON Placeholder_</a>
+          <a onClick={() => { setSelectedProject(Images.imgItem05) }}>Personal Website_</a>
           </div>
         </ProjectsContainer>
       </Content>
