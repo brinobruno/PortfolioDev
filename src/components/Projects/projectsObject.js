40e50fd0ab961b3b368818750b6e5baad76ee030
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-12-20 23:43:09 -0300

    object file with projects improvements semantically

--- src/components/Projects/projectsObject.js
+++ src/components/Projects/projectsObject.js
@@ -5,37 +5,37 @@ import img03 from '../../assets/projectsImg/03-project.png'
 import img04 from '../../assets/projectsImg/04-project.png'
 import img05 from '../../assets/projectsImg/05-project.png'
 
-const Images = {
-  imgItem00: {
+const ProjectsObject = {
+  project00: {
     image: img00,
     projectUrl: 'https://crossfitatos.com.br/primeiropasso',
     repositoryUrl: 'https://github.com/brinobruno/atosbox-landingpage-01'
   },
-  imgItem01: {
+  project01: {
     image: img01,
     projectUrl: 'https://da-so.vercel.app/',
     repositoryUrl: 'https://github.com/brinobruno/DASO-confeitaria'
   },
-  imgItem02: {
+  project02: {
     image: img02,
     projectUrl: 'https://main.d1umw160g6fy01.amplifyapp.com',
     repositoryUrl: 'https://github.com/brinobruno/React-Tasks-List'
   },
-  imgItem03: {
+  project03: {
     image: img03,
     projectUrl: 'https://distracted-beaver-2aa861.netlify.app/',
     repositoryUrl: 'https://github.com/brinobruno/Jogo-da-Cobrinha'
   },
-  imgItem04: {
+  project04: {
     image: img04,
     projectUrl: 'https://json-io.vercel.app/',
     repositoryUrl: 'https://github.com/brinobruno/JSONPlaceholder'
   },
-  imgItem05: {
+  project05: {
     image: img05,
     projectUrl: 'https://meuport.com/',
     repositoryUrl: 'https://github.com/brinobruno/portfolio-MeuPort'
   },
 }
 
-export default Images
+export default ProjectsObject
