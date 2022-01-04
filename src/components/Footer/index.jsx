1528ba822defef768c12205ebb88a4444c6b3f91
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-01-04 08:38:14 -0300

    change year date 2022

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,7 +9,7 @@ export function Footer() {
             <FooterText>brunosantos6ft@gmail.com</FooterText>
             <FooterText><a target='_blank' href="https://meuport.com">Visit my older portfolio &#123;here&#125;</a></FooterText>
           <FooterDivider>
-            <p className='last-text'>Made with React, Styled-Components, a few libs and much study by 2021 ©Bruno Corrêa.</p>
+            <p className='last-text'>Made with React, Styled-Components, a few libs and much study by 2022 ©Bruno Corrêa.</p>
             <Code>&#60;3html&#62;</Code>
           </FooterDivider>
           </Text>
