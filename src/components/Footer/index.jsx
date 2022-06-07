281b0b5073eb952e77c3195fd4854c9a0a5dc2b6
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-07 20:38:55 -0300

    update footer with the addition of blog link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,6 +7,7 @@ export function Footer() {
           <Text>
             <FooterText>São José dos Campos, São Paulo - Brazil</FooterText>
             <FooterText>brunosantos6ft@gmail.com</FooterText>
+            <FooterText><a target='_blank' href="https://natelha.blog">Visit my new personal blog &#123;here&#125;</a></FooterText>
             <FooterText><a target='_blank' href="https://meuport.com">Visit my older portfolio &#123;here&#125;</a></FooterText>
           <FooterDivider>
             <p className='last-text'>Made with React, Styled-Components, a few libs and much study by 2022 ©Bruno Corrêa.</p>
