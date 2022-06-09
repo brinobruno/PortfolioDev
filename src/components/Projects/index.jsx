f198119c8b5785d80282179227535c73de577093
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-08 21:08:16 -0300

    refactor projects section to map function - also converting projectsObject file logic to array instead of encapsulated by object itself

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-
 import { Container, Content, Title, Code, ProjectsContainer, ProjectDisplay, ProjectsList } from './styles'
-
-import ProjectsObject from './projectsObject';
+import { ProjectsObject } from './projectsObject'
 
 export function Projects() {
-  const [selectedProject, setSelectedProject] = useState(ProjectsObject.project00)
+  const [selectedProject, setSelectedProject] = useState(ProjectsObject[0])
 
   return (
     <Container id='projects'>
@@ -14,20 +12,35 @@ export function Projects() {
         <Code>SELECT &#60;project&#62; FROM &#60;projects_list&#62;</Code>
         <ProjectsContainer>
           <ProjectDisplay>
-            <img src={selectedProject.image} alt="Screenshot of the project selected" />
+            <img src={ selectedProject.image } alt="Screenshot of the project selected" />
           <div>
-          <a href={selectedProject.projectUrl}><button>View Project</button></a>
-          <a href={selectedProject.repositoryUrl}><button>View Repository</button></a>
+            <a href={ selectedProject.projectUrl }>
+              <button>
+                View Project
+              </button>
+            </a>
+            <a href={ selectedProject.repositoryUrl }>
+              <button>
+                View Repository
+              </button>
+            </a>
           </div>
           </ProjectDisplay>
 
           <ProjectsList>
-          <a className={selectedProject === ProjectsObject.project00 ? 'selected-item' : ''} onClick={() => { setSelectedProject(ProjectsObject.project00) }}>Crossfit Gym Website_</a>
-          <a className={selectedProject === ProjectsObject.project01 ? 'selected-item' : ''} onClick={() => { setSelectedProject(ProjectsObject.project01) }}>Candy Store Website_</a>
-          <a className={selectedProject === ProjectsObject.project02 ? 'selected-item' : ''} onClick={() => { setSelectedProject(ProjectsObject.project02) }}>React Todo App_</a>
-          <a className={selectedProject === ProjectsObject.project03 ? 'selected-item' : ''} onClick={() => { setSelectedProject(ProjectsObject.project03) }}>Snake Game_</a>
-          <a className={selectedProject === ProjectsObject.project04 ? 'selected-item' : ''} onClick={() => { setSelectedProject(ProjectsObject.project04) }}>JSON Placeholder_</a>
-          <a className={selectedProject === ProjectsObject.project05 ? 'selected-item' : ''} onClick={() => { setSelectedProject(ProjectsObject.project05) }}>Personal Blog_</a>
+            {
+              ProjectsObject.map((item, index) => {
+                return (
+                  <a
+                    key={ item.name }
+                    className={ selectedProject === ProjectsObject[index] ? 'selected-item' : '' }
+                    onClick={() => { setSelectedProject(ProjectsObject[index]) }}
+                  >
+                    { item.name }
+                  </a>
+                )
+              })
+            }
           </ProjectsList>
         </ProjectsContainer>
       </Content>
--- src/components/Projects/projectsObject.js
+++ src/components/Projects/projectsObject.js
@@ -5,37 +5,41 @@ import img03 from '../../assets/projectsImg/03-project.png'
 import img04 from '../../assets/projectsImg/04-project.png'
 import img05 from '../../assets/projectsImg/05-project.png'
 
-const ProjectsObject = {
-  project00: {
+export const ProjectsObject = [
+  {
+    name: 'Crossfit Gym Website_',
     image: img00,
     projectUrl: 'https://crossfitatos.com.br/primeiropasso',
     repositoryUrl: 'https://github.com/brinobruno/atosbox-landingpage-01'
   },
-  project01: {
+  {
+    name: 'Candy Store Website_',
     image: img01,
     projectUrl: 'https://da-so.vercel.app/',
     repositoryUrl: 'https://github.com/brinobruno/DASO-confeitaria'
   },
-  project02: {
+  {
+    name: 'React Todo App_',
     image: img02,
     projectUrl: 'https://main.d1umw160g6fy01.amplifyapp.com',
     repositoryUrl: 'https://github.com/brinobruno/React-Tasks-List'
   },
-  project03: {
+  {
+    name: 'Snake Game_',
     image: img03,
     projectUrl: 'https://distracted-beaver-2aa861.netlify.app/',
     repositoryUrl: 'https://github.com/brinobruno/Jogo-da-Cobrinha'
   },
-  project04: {
+  {
+    name: 'JSON Placeholder_',
     image: img04,
     projectUrl: 'https://json-io.vercel.app/',
     repositoryUrl: 'https://github.com/brinobruno/JSONPlaceholder'
   },
-  project05: {
+  {
+    name: 'Personal Blog_',
     image: img05,
     projectUrl: 'https://natelha.blog/',
     repositoryUrl: 'https://github.com/brinobruno/na-telha'
   },
-}
-
-export default ProjectsObject
+]
