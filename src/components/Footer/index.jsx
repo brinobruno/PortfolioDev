d5f2282a3cc92635ba06e6fb1d2193ea23f59eed
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-12-29 15:08:38 -0300

    chore: Update text

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -31,7 +31,7 @@ export function Footer() {
           <FooterDivider>
             <p className="last-text">
               Made with React, Styled-Components, a few libs and much study by
-              2023 ©Bruno Corrêa.
+              2024 ©Bruno Corrêa.
             </p>
             <Code>&#60;3html&#62;</Code>
           </FooterDivider>
